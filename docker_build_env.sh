9b64868944cfe1ca70649cde03050fdd6646c0f2
Peter Asanov <peter.asanov@aimms.com>
2020-07-24 14:37:06 +0200

    devenvcpp to shell script

--- docker_build_env.sh
+++ docker_build_env.sh
@@ -12,4 +12,7 @@ echo "Building ${1}/essentials..."
 ./utils/docker_build.sh ./essentials ${1}/essentials ${1}/base
 echo "Building ${1}/devenv..."
 ./utils/docker_build.sh ./devenv ${1}/devenv ${1}/essentials
+echo "Building ${1}/devenvcpp..."
+./utils/docker_build.sh ./devenvcpp ${1}/devenvcpp ${1}/devenv
+
 popd
