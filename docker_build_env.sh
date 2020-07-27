bcb845cfa170cac735e3775896306bcb026af5d2
Peter Asanov <peter.asanov@aimms.com>
2020-07-27 12:14:53 +0200

    fixing issues

--- docker_build_env.sh
+++ docker_build_env.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 function build_image() {
-  img_tag=`git describe --tags | sed 's/\-.*//'`
-
+  local img_tag=`git describe --tags | sed 's/\-.*//'`
   local dir_name=$1
+  local img_name=$2
   local name="${2}:${img_tag}"
   pushd ./${dir_name}
 
   echo "Building ${name}..."
-  docker build -t "${name}" --build-arg img_tag=${img_tag} .
+  docker build -t "${name}" .
   docker tag "${name}" "${img_name}:latest"
 
   popd
@@ -17,5 +17,5 @@ function build_image() {
 build_image base aimmspro/native-devenv-base
 build_image essentials aimmspro/native-devenv-essentials
 build_image devenv aimmspro/native-devenv
-build_image devenvcpp aimmspro/native-devenv-cpp
+#build_image devenvcpp aimmspro/native-devenv-cpp
 
