d1a7ede61717ce8d5bd3c4dbef9b98f8ae0fb4a1
Peter Asanov <peter.asanov@aimms.com>
2020-07-27 12:19:33 +0200

    fixing issues

--- devenv/setup.zsh
+++ devenv/setup.zsh
@@ -14,4 +14,4 @@ c az && pip3 install azure-cli
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
     chmod +x ./kubectl && \
     mv ./kubectl /usr/bin/kubectl && \
-    kubectl version --client
+    echo "kubectl version: $(kubectl version --client)"
--- docker_build_env.sh
+++ docker_build_env.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
 function build_image() {
-  local img_tag=`git describe --tags | sed 's/\-.*//'`
+  local img_tag=`git describe --tags | sed 's/\-.*//' || true`
+  if [ ! -e "$img_tag" ]; then
+    img_tag=latest
+  fi
+
   local dir_name=$1
   local img_name=$2
   local name="${2}:${img_tag}"
