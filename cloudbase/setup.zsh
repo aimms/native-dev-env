ef7e61ef95f54465014b6c52373140fceec29dbe
Peter Asanov <peter.asanov@aimms.com>
2020-08-03 21:39:42 +0200

    some dependencies

--- cloudbase/setup.zsh
+++ cloudbase/setup.zsh
@@ -5,6 +5,8 @@ source ~/.zshrc
 pyenv global 3.8.3 && c az
 pip3 install wheel
 pip3 install azure-cli
+pip3 install jmespath
+pip3 install typed-argument-parser
 
 curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt`/bin/linux/amd64/kubectl && \
     chmod +x ./kubectl && \
