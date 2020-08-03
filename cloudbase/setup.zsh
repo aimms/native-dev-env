2d8d07c618ca2fb2ed27fa4f62d698c52bca0546
Peter Asanov <peter.asanov@aimms.com>
2020-08-04 00:37:09 +0200

    terraform

--- cloudbase/setup.zsh
+++ cloudbase/setup.zsh
@@ -13,6 +13,14 @@ curl -LO https://storage.googleapis.com/kubernetes-release/release/`curl -s http
     echo "kubectl version: $(kubectl version --client)"
 
 
+pushd /usr/bin
+tf_version=0.12.28
+terraform_zip=terraform_${tf_version}_linux_amd64.zip
+curl -LO https://releases.hashicorp.com/terraform/${tf_version}/${terraform_zip}
+unzip ${terraform_zip}
+rm -f ${terraform_zip}
+popd
+
 cat << 'EOF' >> ~/.zshrc
 
 alias k=kubectl
