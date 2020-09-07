413fce8ad4b3ad926300546b1038909e5d1d646c
Peter Asanov <peter.asanov@aimms.com>
2020-09-07 16:25:56 +0200

    build_env.sh

--- build_env.sh
+++ build_env.sh
@@ -1,37 +1,74 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ $# -lt 1 || "$1" == "-h" || "$1" == "--help" || ( $# -eq 2 && "$2" != "--upload" ) || $# -gt 2  ]]; then
-    echo "Usage: $0 <version>"
-    exit 1
+#if [[ $# -lt 1 || "$1" == "-h" || "$1" == "--help" || ( $# -eq 2 && "$2" != "--upload" ) || $# -gt 2  ]]; then
+#    echo "Usage: $0 <version>"
+#    exit 1
+#fi
+
+#version="$1"
+
+ubuntu_ver=20.04
+
+if [ ! -e $1 ]; then
+	container="$1"
+else
+	container=$(buildah from ubuntu:$ubuntu_ver)
 fi
 
-version="$1"
+echo "container id: $container"
 
-function build_image() {
-  local dir_name=$1
-  local img_name=$2
-  local name="${img_name}:latest"
-  local v_name="${img_name}:${version}"
-  local upload="$3"
 
-  if [  "$4" != "" ]; then
-    local build_arg="--build-arg BASE=$4"
-  fi
 
-  echo "Building ${name}..."
-  docker build -t ${name} $build_arg ./${dir_name}
-  docker tag ${name} "${v_name}"
+set -e
 
-  if [ "${upload}" == "--upload" ]; then
-    echo "Uploading image: ${img_name}"
-    docker push "${v_name}"
-    docker push "${name}"
-  fi
+script_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+pushd "$script_dir" || exit
+
+ubuntu_ver=20.04
+
+# essentials
+
+b(){
+  buildah run --runtime /usr/bin/crun $container -- $@
 }
 
-build_image essentials aimmspro/devenv-essentials "$2"
-build_image python aimmspro/devenv-python "$2"
-build_image cloud aimmspro/devenv-cloud "$2"
-build_image theming aimmspro/devenv-cloud-theming "$2" "cloud"
-build_image native aimmspro/devenv-native "$2"
-build_image theming aimmspro/devenv-native-theming "$2" "native"
+i='apt install -y --no-install-recommends'
+
+
+buildah config --env DEBIAN_FRONTEND=noninteractive $container
+
+b apt update
+b apt upgrade -y
+b $i zsh vim tmux wget curl fontconfig git zip git ca-certificates
+b $i build-essential libssl-dev zlib1g-dev libbz2-dev \
+    libreadline-dev libsqlite3-dev  libncurses5-dev libncursesw5-dev \
+    xz-utils libffi-dev liblzma-dev python-openssl
+b $i llvm clang-10 libc++-dev libc++abi-dev
+
+b update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-10 100
+b update-alternatives --install /usr/bin/clang clang /usr/bin/clang-10 100
+b update-alternatives --install /usr/bin/cc cc /usr/bin/clang 100
+b update-alternatives --install /usr/bin/c++ c++ /usr/bin/clang++ 100
+b rm -rf /var/lib/apt/lists/*
+
+#container=$(buildah from aimmspro/devenv-essentials)
+
+buildah config --env GIT_EDITOR=vim $container
+
+buildah config --env PYTHON_VERSION=3.8.5 $container
+buildah config --env PYENV_VIRTUALENV_DISABLE_PROMPT=1 $container
+
+b bash -c 'curl https://pyenv.run | bash'
+b bash -c 'MAKE_OPTS="V=1 -j`grep -c ^processor /proc/cpuinfo`" LLVM_PROFDATA=/usr/bin/llvm-profdata-10 CONFIGURE_OPTS=--enable-optimizations /root/.pyenv/bin/pyenv install $PYTHON_VERSION --verbose'
+
+
+buildah copy --chown root $container $script_dir/assets /tmp
+b mv /tmp/.zshrc /root
+b /tmp/state0.zsh && rm -f /tmp/stage0.zsh
+
+buildah config --entrypoint /bin/zsh $container
+
+buildah commit $container aimmspro/devenv-cloud
+
+
+popd || exit # script_dir
