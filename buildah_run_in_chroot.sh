57e6c02143288384ac2358d1d9a598de95a37e92
Peter Asanov <peter.asanov@aimms.com>
2020-09-08 16:19:05 +0200

    fixing issues:

---
+++ buildah_run_in_chroot.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+container=$1
+payload=$2
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+
+mnt=$(buildah mount $container)
+cp /etc/resolv.conf $mnt/etc/
+mkdir -p $mnt/tmp/host
+mount --bind $script_dir $mnt/tmp/host
+umount $mnt/tmp/host
+
+chroot $mnt /tmp/host/$payload
+
