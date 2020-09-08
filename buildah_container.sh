df4880136595e7b6ff710b2da6f815aa467791ac
Peter Asanov <peter.asanov@aimms.com>
2020-09-08 15:46:38 +0200

    fixing issues

--- buildah_container.sh
+++ buildah_container.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
@@ -6,4 +6,4 @@ script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 mkdir -p $HOME/mycontainers
 
 $1 run --runtime crun --rm --net=host --security-opt label=disable --security-opt seccomp=unconfined --device /dev/fuse:rw -v $HOME/mycontainers:/var/lib/containers:Z -v $script_dir:/code:Z  -it docker://quay.io/buildah/stable:latest /code/build_env.sh
- 
+
