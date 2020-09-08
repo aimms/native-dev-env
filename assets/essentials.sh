a0a4c44e456070af5ac3d1b4569b877e24987c85
Peter Asanov <peter.asanov@aimms.com>
2020-09-08 19:05:53 +0200

    fixing issues

--- assets/essentials.sh
+++ assets/essentials.sh
@@ -9,6 +9,8 @@ DEBIAN_FRONTEND=noninteractive \
 sed -i -e 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && \
   dpkg-reconfigure --frontend=noninteractive locales && update-locale LANG=en_US.UTF-8
 
+export HOME=/root 
+
 curl https://pyenv.run | bash
 
 cp /host/assets/.zshrc ~/.zshrc
