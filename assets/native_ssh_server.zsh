8580993eaa326affa1465090c50b74e90b19fb44
Peter Asanov <peter.asanov@aimms.com>
2020-09-09 01:30:34 +0200

    fixing issues

--- assets/native_ssh_server.zsh
+++ assets/native_ssh_server.zsh
@@ -2,6 +2,8 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
+BUILD_USER=builderboy
+
 apt update
 apt install -y --no-install-recommends openssh-server gdb rsync sudo
 
@@ -9,13 +11,23 @@ apt install -y --no-install-recommends openssh-server gdb rsync sudo
 mkdir -p /var/run/sshd
 echo 'PasswordAuthentication yes' >> /etc/ssh/sshd_config && ssh-keygen -A
 
-# create our 'builderboy' user
-useradd -m -d /home/builderboy -s /bin/bash -G sudo builderboy && echo "builderboy:builderboy" | chpasswd
+# create our '$BUILD_USER' user
+useradd -m -d /home/$BUILD_USER -s /bin/bash -G sudo $BUILD_USER && echo "$BUILD_USER:$BUILD_USER" | chpasswd
 
-# let builderboy use sudo with requiring a password
+# let $BUILD_USER use sudo with requiring a password
 sed -i /etc/sudoers -re 's/^%sudo.*/%sudo ALL=(ALL:ALL) NOPASSWD: ALL/g'
 sed -i /etc/sudoers -re 's/^root.*/root ALL=(ALL:ALL) NOPASSWD: ALL/g'
 sed -i /etc/sudoers -re 's/^#includedir.*/## **Removed the include directive** ##"/g'
-echo "builderboy ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+echo "$BUILD_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+chown root /var/run/sshd
+chmod 744 /var/run/sshd
+
+mv /root/.pyenv /home/$BUILD_USER
+mv /root/.zshrc /home/$BUILD_USER
+
+chown -R $BUILD_USER /home/$BUILD_USER
+
+chsh -s /bin/zsh $BUILD_USER
 
 apt autoremove -y && rm -rf /var/lib/apt/lists/*
