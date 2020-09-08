c5bc14e013f12fbe1ab82b57adb35ed747e44757
Peter Asanov <peter.asanov@aimms.com>
2020-09-08 16:32:15 +0200

    fixing issues

--- buildah_run_in_chroot.sh
+++ buildah_run_in_chroot.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+if [[ $# -lt 2 || "$1" == "-h" || "$1" == "--help" || $# -gt 2 ]]; then
+  echo "Usage: $0 <buildah container name> <command>"
+  echo " e.g.: $0 build build_env.sh 1.5"
+  exit 1
+fi
+
+
 container=$1
-payload=$2
+shift
 
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 mnt=$(buildah mount $container)
 cp /etc/resolv.conf $mnt/etc/
-mkdir -p $mnt/tmp/host
+mkdir -p $mnt/host
 mount --bind $script_dir $mnt/tmp/host
 
-chroot $mnt /tmp/host/$payload
-umount $mnt/tmp/host
-rm -rf $mnt/tmp/host
+chroot $mnt/host "@$"
+umount $mnt/host
