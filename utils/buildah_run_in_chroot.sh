324825ed87838ac472c367f1094a16ab80e9aec5
Peter Asanov <peter.asanov@aimms.com>
2020-09-13 12:22:09 +0200

    fixing issues

--- utils/buildah_run_in_chroot.sh
+++ utils/buildah_run_in_chroot.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-if [[ $# -lt 2 || "$1" == "-h" || "$1" == "--help" ]]; then
-  echo "Usage: $0 <buildah existing container name> <relative script path> <script arguments>"
+if [[ $# -lt 3 || "$1" == "-h" || "$1" == "--help" ]]; then
+  echo "Usage: $0 <buildah existing container name> <script_dir> <relative script path> <script arguments>"
   exit 1
 fi
 
 container=$1
 shift
+script_dir=$1
+shift
 payload=$1
 shift
-
-script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
 mnt=$(buildah mount $container)
 cp /etc/resolv.conf $mnt/etc/
