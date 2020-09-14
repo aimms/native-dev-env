83bda24141155a9ec8f758b9cc12ec43d21e9efe
Peter Asanov <peter.asanov@aimms.com>
2020-09-15 01:03:24 +0200

    progress

--- stages/00_essentials.sh
+++ stages/00_essentials.sh
@@ -7,10 +7,23 @@ export DEBIAN_FRONTEND=noninteractive
 apt update
 apt install -y --no-install-recommends fakeroot
 
+fakeroot -- mknod -m 600 /dev/console c 5 1
 fakeroot -- mknod -m 666 /dev/null c 1 3
-fakeroot -- mknod /dev/zero c 1 5
-fakeroot -- mknod /dev/random c 1 8
-fakeroot -- mknod /dev/urandom c 1 9
+fakeroot -- mknod -m 622 /dev/console c 5 1
+fakeroot -- mknod -m 666 /dev/null c 1 3
+fakeroot -- mknod -m 666 /dev/zero c 1 5
+fakeroot -- mknod -m 666 /dev/ptmx c 5 2
+fakeroot -- mknod -m 666 /dev/tty c 5 0
+fakeroot -- mknod -m 444 /dev/random c 1 8
+fakeroot -- mknod -m 444 /dev/urandom c 1 9
+
+fakeroot -- ln -sv /proc/self/fd /dev/fd
+fakeroot -- ln -sv /proc/self/fd/0 /dev/stdin
+fakeroot -- ln -sv /proc/self/fd/1 /dev/stdout
+fakeroot -- ln -sv /proc/self/fd/2 /dev/stderr
+fakeroot -- ln -sv /proc/kcore /dev/core
+fakeroot -- mkdir -v /dev/pts
+fakeroot -- mkdir -v /dev/shm
 
 # install basic tools
 apt upgrade -y && apt install -y --no-install-recommends \
